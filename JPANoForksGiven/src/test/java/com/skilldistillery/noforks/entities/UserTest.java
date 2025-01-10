7f800bde0ae45558a4a252dab389331218f19dc4
William Brown <wsbrown.colo@gmail.com>
2025-01-10 11:32:50 -0700

    Stub DB, create user table/entity, test Hibernate config

--- JPANoForksGiven/src/test/java/com/skilldistillery/noforks/entities/UserTest.java
+++ JPANoForksGiven/src/test/java/com/skilldistillery/noforks/entities/UserTest.java
@@ -1,14 +1,54 @@
 package com.skilldistillery.noforks.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class UserTest {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
+class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
+	
+
+	@BeforeAll
+	 static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPANoForksGiven");
+	 }
+
+
+	 @AfterAll
+	 static void tearDownAfterClass() throws Exception {
+		 emf.close();
+	 }
+
+
+	 @BeforeEach
+	 void setUp() throws Exception {
+		 em = emf.createEntityManager();
+		 user = em.find(User.class, 1);
+	 }
+
+
+	 @AfterEach
+	 void tearDown() throws Exception {
+		 em.close();
+		 user = null;
+	 }
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_User_entity_mapping() {
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
 	}
 
 }
