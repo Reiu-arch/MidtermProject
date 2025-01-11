1071737b3dcc3154439050b558b56f9de6137095
William Brown <wsbrown.colo@gmail.com>
2025-01-11 12:00:02 -0700

    MealType entity complete, MealTypeTest passed

---
+++ JPANoForksGiven/src/test/java/com/skilldistillery/noforks/entities/MealTypeTest.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.noforks.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class MealTypeTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private MealType mealType;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPANoForksGiven");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		mealType = em.find(MealType.class, 3);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		mealType = null;
+	}
+
+	@Test
+	void test_MealType_entity_mapping() {
+		assertNotNull(mealType);
+//		assertEquals("Breakfast", mealType.getName());
+//		assertEquals("Lunch", mealType.getName());
+		assertEquals("Dinner", mealType.getName());
+//		assertEquals("Snack", mealType.getName());
+	}
+
+}
