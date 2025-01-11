8c3c8e367685979cd2a08ee4edbea978ef47c0bd
William Brown <wsbrown.colo@gmail.com>
2025-01-11 11:12:08 -0700

    Plan entity complete, PlanTest passed

--- JPANoForksGiven/src/main/java/com/skilldistillery/noforks/entities/Plan.java
+++ JPANoForksGiven/src/main/java/com/skilldistillery/noforks/entities/Plan.java
@@ -1,5 +1,104 @@
 package com.skilldistillery.noforks.entities;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Plan {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="create_date")
+	private LocalDateTime createDate;
+	
+	@Column(name="last_update")
+	private LocalDateTime lastUpdate;
+	private String notes;
+	
+	@Column(name="user_id")
+	private Integer userId;
+	
+	@Column(name="image_url")
+	private String imageUrl;
+	
+	
+
+	public Plan() {
+		super();
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+	public LocalDateTime getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(LocalDateTime lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Plan other = (Plan) obj;
+		return id == other.id;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Plan [id=" + id + ", createDate=" + createDate + ", lastUpdate=" + lastUpdate + ", notes=" + notes
+				+ ", userId=" + userId + ", imageUrl=" + imageUrl + "]";
+	}
 
 }
--- JPANoForksGiven/src/test/java/com/skilldistillery/noforks/entities/PlanTest.java
+++ JPANoForksGiven/src/test/java/com/skilldistillery/noforks/entities/PlanTest.java
@@ -2,7 +2,6 @@ package com.skilldistillery.noforks.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -14,43 +13,40 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-class UserTest {
+class PlanTest {
 	
 	private static EntityManagerFactory emf;
 	private EntityManager em;
-	private User user;
-	
+	private Plan plan;
 
+	
 	@BeforeAll
-	 static void setUpBeforeClass() throws Exception {
+	static void setUpBeforeClass() throws Exception {
 		emf = Persistence.createEntityManagerFactory("JPANoForksGiven");
-	 }
+	}
 
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
 
-	 @AfterAll
-	 static void tearDownAfterClass() throws Exception {
-		 emf.close();
-	 }
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		plan = em.find(Plan.class, 1);
+	}
 
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		plan = null;
+	}
 
-	 @BeforeEach
-	 void setUp() throws Exception {
-		 em = emf.createEntityManager();
-		 user = em.find(User.class, 1);
-	 }
-
-
-	 @AfterEach
-	 void tearDown() throws Exception {
-		 em.close();
-		 user = null;
-	 }
-	
 	@Test
-	void test_User_entity_mapping() {
-		assertNotNull(user);
-		assertEquals("admin", user.getUsername());
-		assertTrue(user.getEnabled());
+	void test_Plan_entity_mapping() {
+		assertNotNull(plan);
+		assertEquals(2025, plan.getCreateDate().getYear());
+		
 	}
 
 }
