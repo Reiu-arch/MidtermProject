fbf8300b87e82a083759a4736d043bbdb1a65310
Leslie <leslie_aldaz@hotmail.com>
2025-01-11 12:33:27 -0700

    recipeReview entity and recipeReviewId (embedded class) completed and Junit test passed

---
+++ JPANoForksGiven/src/test/java/com/skilldistillery/noforks/entities/RecipeReviewTest.java
@@ -0,0 +1,58 @@
+package com.skilldistillery.noforks.entities;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class RecipeReviewTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private RecipeReview recipeReview;
+	
+
+	@BeforeAll
+	 static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPANoForksGiven");
+	 }
+
+
+	 @AfterAll
+	 static void tearDownAfterClass() throws Exception {
+		 emf.close();
+	 }
+
+
+	 @BeforeEach
+	 void setUp() throws Exception {
+		 em = emf.createEntityManager();
+		 RecipeReviewId rId = new RecipeReviewId();
+		 rId.setRecipeId(1);
+		 rId.setUserId(1);
+		 recipeReview = em.find(RecipeReview.class, rId);
+		 
+	 }
+
+
+	 @AfterEach
+	 void tearDown() throws Exception {
+		 em.close();
+		 recipeReview = null;
+	 }
+	
+	@Test
+	void test_RecipeReview_entity_mapping() {
+		assertNotNull(recipeReview);
+		assertNull(recipeReview.getRemarks());
+	}
+
+}
