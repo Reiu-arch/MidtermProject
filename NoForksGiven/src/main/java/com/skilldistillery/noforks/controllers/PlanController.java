efef05169ada8eae2b332316eb612d65aa9d80d6
Reiu-arch <vidaldominguez60@gmail.com>
2025-01-14 09:53:25 -0700

    nav bar mapped out, plan stubbed out

--- NoForksGiven/src/main/java/com/skilldistillery/noforks/controllers/PlanController.java
+++ NoForksGiven/src/main/java/com/skilldistillery/noforks/controllers/PlanController.java
@@ -1,9 +1,26 @@
 package com.skilldistillery.noforks.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.skilldistillery.noforks.data.PlanDAO;
+import com.skilldistillery.noforks.entities.Plan;
 
 @Controller
 public class PlanController {
 	
+	@Autowired
+	private PlanDAO planDao;
 
+	@GetMapping(path = "createplans.do")
+	public String gocreateplan(Model model) {
+		return "createplans";
+	}
+	
+	@GetMapping(path = "myplan.do")
+	public String goplan(Model model) {
+		return "myplan";
+	}
 }
---
+++ NoForksGiven/src/main/java/com/skilldistillery/noforks/data/PlanDAO.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.noforks.data;
+
+import com.skilldistillery.noforks.entities.Plan;
+import com.skilldistillery.noforks.entities.Recipe;
+import com.skilldistillery.noforks.entities.User;
+
+public interface PlanDAO {
+
+Recipe addPlan(Plan plan, User user);
+	
+	boolean deleteByPlanId(int planId, User user);
+}
