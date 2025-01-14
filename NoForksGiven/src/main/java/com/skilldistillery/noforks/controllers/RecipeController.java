9eca8dc33b151d424d4a3151c871047ed203688b
William Brown <wsbrown.colo@gmail.com>
2025-01-14 09:38:45 -0700

    recipeController created, moved methods from userController

---
+++ NoForksGiven/src/main/java/com/skilldistillery/noforks/controllers/RecipeController.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.noforks.controllers;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.skilldistillery.noforks.data.RecipeDAO;
+import com.skilldistillery.noforks.entities.Recipe;
+import com.skilldistillery.noforks.entities.User;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class RecipeController {
+	
+	@Autowired
+	private RecipeDAO recipeDao;
+
+	@GetMapping(path = "createrecipe.do")
+	public String gocreaterecipe(Model model) {
+		return "createrecipe";
+	}
+	
+	@PostMapping(path = "createrecipe.do")
+	public String createRecipe(Recipe recipe, Model model, HttpSession session) {
+		try {
+			User loggedInUser = (User) session.getAttribute("loggedInUser");
+			if (loggedInUser == null) {
+				model.addAttribute("errorMessage", "You need to log in to create a recipe");
+				return "login";
+			} 
+//			recipe.setUser(loggedInUser);
+			recipe.setCreateDate(LocalDateTime.now());
+			recipe.setLastUpdate(LocalDateTime.now());
+			
+			recipeDao.addRecipe(recipe, loggedInUser);
+				
+			model.addAttribute("sucessMessage", "Recipe created Successfully");
+			return "redirect:browseResults.do";	
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("errorMessage", "An error occured while saving your recipe.");
+			return "createrecipe";
+		}
+	}
+	
+}
